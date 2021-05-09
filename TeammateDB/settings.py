6155f6a709aae72e58a0cbe9af5e2a6d6c51c3fc
Ebrar Bilgili <ebrarbilgili@Ebrar-MacBook-Pro.local>
2021-05-09 12:30:43 +0300

    settings fixed

--- TeammateDB/settings.py
+++ TeammateDB/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$yrr^d2h03vmv%4r)0nt6jc#0-vp#68gy*mcunln3tb!nr-ot^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['teammateapp.herokuapp.com', '127.0.0.1']
 
